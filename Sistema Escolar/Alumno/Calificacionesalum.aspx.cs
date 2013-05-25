ee52c29315adc31de412aaf50113292005795d75
Heriberto Cantú <es.beto@gmail.com>
2013-05-25 09:39:17 -0500

    agregar comentarios de como conectar a la bd

--- Sistema Escolar/Alumno/Calificacionesalum.aspx.cs
+++ Sistema Escolar/Alumno/Calificacionesalum.aspx.cs
@@ -9,8 +9,21 @@ namespace Sistema_Escolar.Alumno
 {
     public partial class Calificacionesalum : System.Web.UI.Page
     {
+        // Conexicon
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Conexion con el string
+
+            // Operaciones a la base de datos 
+            // Select nombre, matricula, carrera,esucle, etc where id = 123456
+
+            //
+
+
+
+
+
 
         }
     }
