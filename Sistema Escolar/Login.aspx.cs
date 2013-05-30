9cb9c474db1fdde6802151353f88d8384a761c73
Heriberto Cantú <es.beto@gmail.com>
2013-05-30 01:19:03 -0500

    SQL root files

--- Sistema Escolar/Login.aspx.cs
+++ Sistema Escolar/Login.aspx.cs
@@ -24,7 +24,7 @@ namespace Sistema_Escolar
             SqlDataAdapter da = new SqlDataAdapter();
             DataTable dtResult = new DataTable();
 
-            SqlCommand sqlCommand = new SqlCommand("select ID_tipousuario from Usuarios where usuario = '"+txtusu.Text+"' and password ='"+txtpass.Text+"'", sqlCon);
+            SqlCommand sqlCommand = new SqlCommand("select ID_tipousuario,id_alumno,id_maestro,id_admin,id_tesoreria from Usuarios where usuario = '"+txtusu.Text+"' and password ='"+txtpass.Text+"'", sqlCon);
             sqlCommand.CommandType = CommandType.Text;
             sqlCommand.CommandTimeout = 4000;
 
@@ -35,22 +35,25 @@ namespace Sistema_Escolar
             da.Fill(dtResult);
             //sqlCommand.ExecuteNonQuery(); para update,inset,delete
             sqlCon.Close();
-
+            
             if (dtResult.Rows.Count > 0)//si debuelve mas de un registro si existe el usuario
+               
             {
                 String tipousuario = dtResult.Rows[0]["ID_tipousuario"].ToString();
-
+               
                 if (tipousuario == "0")
                 {
+                    Session["idAlumno"] = dtResult.Rows[0]["id_alumno"].ToString();
                     Response.Redirect("/Alumno/Datosalum.aspx");
+                    
                 }
                 else if (tipousuario == "1")
                 {
-                    Response.Redirect("/Admin/alta_alum_admin.aspx");
+                    Response.Redirect("/Maestro/alta_consulta.aspx");
                 }
                 else if (tipousuario == "2")
                 {
-                    Response.Redirect("/Maestro/alta_consulta.aspx");
+                    Response.Redirect("/Admin/alta_alum_admin.aspx");
                 }
                 else
                 {
