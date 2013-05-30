b4410d6150eafe4875e51346764173d8700bbca6
Heriberto Cantú <es.beto@gmail.com>
2013-05-30 01:04:33 -0500

    Admin .cs SQL strings

--- Sistema Escolar/Admin/Salones_altasybajasgenerales.aspx.cs
+++ Sistema Escolar/Admin/Salones_altasybajasgenerales.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 namespace Sistema_Escolar.Admin
 {
     public partial class Salones_altasybajasgenerales : System.Web.UI.Page
     {
+        string conexionest = ConfigurationManager.ConnectionStrings["conexionsql"].ToString();
         protected void Page_Load(object sender, EventArgs e)
         {
 
